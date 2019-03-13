8cc61c92f8b6a64928ae424dcdcad455a480bf5d
tigerwer@126.com <tigerwer@126.com>
2019-03-13 10:21:33 +0800

    init can run

--- shop-api/src/main/java/com/xgit/bj/shop/beans/param/goods/GoodsParam.java
+++ shop-api/src/main/java/com/xgit/bj/shop/beans/param/goods/GoodsParam.java
@@ -1,6 +1,11 @@
 package com.xgit.bj.shop.beans.param.goods;
 
+import com.xgit.bj.shop.beans.vo.goods.GoodsAttributeValueVO;
+import com.xgit.bj.shop.beans.vo.goods.GoodsFullReductionVO;
+import com.xgit.bj.shop.beans.vo.goods.GoodsLadderVO;
 import com.xgit.bj.shop.beans.vo.goods.GoodsVO;
+import com.xgit.bj.shop.beans.vo.goods.MemberPriceVO;
+import com.xgit.bj.shop.beans.vo.goods.SkuStockVO;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.ToString;
@@ -10,16 +15,16 @@ import java.util.List;
 @Data
 @ToString
 public class GoodsParam extends GoodsVO {
-//    @ApiModelProperty("商品阶梯价格设置")
-//    private List<GoodsLadder> productLadderList;
-//    @ApiModelProperty("商品满减价格设置")
-//    private List<PmsProductFullReduction> productFullReductionList;
-//    @ApiModelProperty("商品会员价格设置")
-//    private List<PmsMemberPrice> memberPriceList;
-//    @ApiModelProperty("商品的sku库存信息")
-//    private List<PmsSkuStock> skuStockList;
-//    @ApiModelProperty("商品参数及自定义规格属性")
-//    private List<PmsProductAttributeValue> productAttributeValueList;
+    @ApiModelProperty("商品阶梯价格设置")
+    private List<GoodsLadderVO> productLadderList;
+    @ApiModelProperty("商品满减价格设置")
+    private List<GoodsFullReductionVO> goodsFullReductionVOList;
+    @ApiModelProperty("商品会员价格设置")
+    private List<MemberPriceVO> memberPriceList;
+    @ApiModelProperty("商品的sku库存信息")
+    private List<SkuStockVO> skuStockList;
+    @ApiModelProperty("商品参数及自定义规格属性")
+    private List<GoodsAttributeValueVO> goodsAttributeValueVOList;
 //    @ApiModelProperty("专题和商品关系")
 //    private List<CmsSubjectProductRelation> subjectProductRelationList;
 //    @ApiModelProperty("优选专区和商品的关系")
